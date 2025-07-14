e1c3559437e6e623828bedc2258cfc01134a6ab6
Shea <zrdl15@163.com>
2025-07-14 17:03:53 +0800

    0714 打包配置

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import {URL,fileURLToPath} from "node:url"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 路径是 https://<用户名>.github.io/<仓库名>/ ，所以 base 设为仓库名
+  base: '/TOPIK/',
   resolve: {
     alias:{
           "@":fileURLToPath(new URL("./src",import.meta.url))
